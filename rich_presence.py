958151de55a62a30f824a10d252c320e8e28b27d
Zetsuki <zetsukiOG@gmail.com>
2025-05-08 13:24:38 +0200

    Initial script done, added requirements

---
+++ rich_presence.py
@@ -0,0 +1,16 @@
+from pypresence import Presence
+import time
+
+CLIENT_ID="1369991638126166087"
+rpc = Presence(CLIENT_ID)
+rpc.connect()
+
+rpc.update(
+    state="In a race",
+    details="Richard Burns Rally (Rallysimfans)",
+    start=time.time()
+)
+
+print("Rich Presence for Richard Burn Rally activated. Ctrl+C to quit.")
+while True:
+    time.sleep(15)
